345cad84a36535ea062b2dc195c987ae781f9528
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2013-03-01 16:44:48 +0100

    Replaced runOrRaise with dmenu

--- xmonad.hs
+++ xmonad.hs
@@ -67,12 +67,14 @@ main = do
             , ("M-a", sendMessage MirrorExpand)                       -- (6)
             , ("M-m", viewEmptyWorkspace)       -- (6)
             , ("M-n", refresh)                  -- (7)
-            , ("M-p", runOrRaisePrompt defaultXPConfig)
+            , ("M-p", spawn myDmenu)
             , ("M-z", sendMessage MirrorShrink)                       -- (6)
             ]
         `additionalKeys`
         -- NOTE: planeKeys requires xmonad-0.9 or greater
         M.toList (planeKeys mod4Mask GConf Finite)
+
+myDmenu = "exe=`dmenu_run ` && eval \"exec $exe\""
 
 
 myManageHook :: [ManageHook]
